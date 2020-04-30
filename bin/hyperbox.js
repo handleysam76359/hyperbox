0ec6baeebc14156bcf9d25dbcd3769c824bf8634
Brecht Savelkoul <brecht.savelkoul@alumni.lse.ac.uk>
2020-04-30 19:32:39 +0200

    Port SMTP handling to `usemail`

--- bin/hyperbox.js
+++ bin/hyperbox.js
@@ -1,38 +1,20 @@
-var { Connection } = require('../lib/connect')
 var { number, nothing, string } = require('stdopt')
 var { run, text } = require('stdrun')
-var { readFile } = require('fs')
-var { start } = require('../lib/smtp')
+var Connection = require('../lib/connect')
+var hyperbox = require('../lib/hyperbox')
+var usemail = require('usemail')
 
-async function hyperbox (opts = {}) {
-  var cfgPath = string(opts.config).or(opts.c).or('./config.json').value()
+async function main (opts = {}) {
+  var config = string(opts.config).or(opts.c).or('./config.json').value()
   var port = number(opts.port).or(opts.p).or(nothing).value()
 
-  var config = await readConfig(cfgPath)
-  var hd = await Connection.create(require('../config'))
-  var address = await start({ hd, port })
+  var dat = await Connection.configure(config)
+  var server = usemail({ authOptional: true })
+  server.use(usemail.parse())
+  server.use(hyperbox({ dat }))
+  await server.listen(port)
 
-  return text`Hyperbox started on port ${address.port}`
+  return text`Hyperbox started on port ${server.port}`
 }
 
-run(hyperbox)
-
-/**
- * Helpers
- */
-async function readConfig (path) {
-  return new Promise((resolve, reject) => {
-    readFile(path, (err, cfg) => {
-      if (err) {
-        console.warn('Failed to read config: ' + path)
-        resolve({})
-      }
-
-      try {
-        resolve(JSON.parse(cfg))
-      } catch (err) {
-        reject(err)
-      }
-    })
-  })
-}
+run(main)
